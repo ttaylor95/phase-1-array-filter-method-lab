cc0b8b080f5b43febb434ad263f754a6a39c4a07
Teanna Taylor <teannamtaylor95@gmail.com>
2022-06-14 13:35:09 -0700

    commit tree 12d969e88e43cf0b32febaebf888f275355112ac

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Anette', 'Sarah', 'Bobby'];
+//function one
+
+function findMatching(drivers, string) {
+
+const driva = drivers.filter(element => element === string);
+
+return driva;
+
+}
+//function two
+
+
+
+//function three
